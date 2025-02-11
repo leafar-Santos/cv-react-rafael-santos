76c2c073085bcaf7a78984254874d8f4a5320251
leafar-Santos <leafarccomputacao@gmail.com>
2025-02-11 11:35:46 -0300

    Ajuste descrição

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -47,7 +47,7 @@ function Experience() {
     {
       company: "Intelipost",
       icon: Intelipost,
-      role: "Analista de qualidade de sistemas Sênior",
+      role: "Analista de qualidade Sênior",
       period: "Novembro de 2020 - Agosto de 2021",
       location: "São Paulo",
       responsibilities: [
